692ada1c9740fdd6133359faf98bea2c17eb6b12
Tal <tgoren01@gmail.com>
2025-12-25 14:38:23 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import SignIn from './components/Pages/SignIn';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <SignIn/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Pages/SignIn.tsx
@@ -0,0 +1,12 @@
+const SignIn = () => {
+    return (
+      <>
+        <label>Email</label>
+        <input></input>
+        <label>Password</label>
+        <input></input>
+      </>
+    );
+}
+
+export default SignIn;
